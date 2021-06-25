9c472ddd4cad0835533445c9e338bebc1be6964a
Mert Erden <merden01@eecs.tufts.edu>
2021-06-25 10:25:46 -0400

    moved dir structure

--- t_map/garbanzo/edgelist.py
+++ t_map/garbanzo/edgelist.py
@@ -1,13 +1,15 @@
 import networkx as nx
+from typing import List
 from t_map.garbanzo.garbanzo import Garbanzo
 from t_map.gene.gene import Gene
 
 
 class EdgeListGarbanzo(Garbanzo):
     def __init__(self, graph_path: str, gene_path: str):
-        self._graph = self._read_graph_from_path(graph_path)
         self._graph_path = graph_path
         self._gene_path = gene_path
+        self._graph = self._read_graph_from_path(graph_path)
+        self._genes = self._read_genes_from_path(gene_path)
 
     @property
     def graph(self) -> nx.Graph:
@@ -22,7 +24,15 @@ class EdgeListGarbanzo(Garbanzo):
         return self._gene_path
 
     def get(self, i: int) -> Gene:
-        raise NotImplementedError
+        return self._genes[i]
 
     def _read_graph_from_path(self, path: str) -> nx.Graph:
         return nx.read_edgelist(path)
+
+    def _read_gene_from_path(self, path: str) -> List[Gene]:
+        genes = []
+        with open(path) as f:
+            for line in f.readlines():
+                gene = line.strip()
+                genes.append(Gene(name=gene))
+        return genes
