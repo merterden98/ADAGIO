f968b5183e560e393949e3dbc4997912c6a91a56
Kyla Levin <kyla.levin@gmail.com>
2021-07-12 10:48:55 -0400

    Clap update

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,42 @@ use std::fs::File;
 use std::io::prelude::*;
 use pyo3::prelude::*; // Python importing
 use clap::Clap; // Command line handling
+use clap::{Arg, App};
 
-// Struct with all command line args
-#[derive(Clap, Debug)]
-#[clap(name = "args")]
-struct Args {
-    // Algorithm to run (eg. rwr)
-    #[clap(short, long)]
-    name: String,
+// extern crate clap;
 
-    // Data set to run (.tsv file)
-    #[clap(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[clap(short, long, default_value = "1")]
-    count: u8,
-}
-
-fn main() -> PyResult<()> {
+fn main() -> Result<T, E> {
 
     /* Use a clap package to handle command line parsing and put into a struct */
 
-    Python::with_gil(|py| {
-        let GarbanzoR = PyModule::import(py, "Garbanzo")?;
-        let RwrR = PyModule::import(py, "RandomWalkWithRestart")?;
-        // Testing purposes
-        let testFetaR = PyModule::import(py, "test_feta")?;
+    let matches = App::new("TMap Program")
+        .author("Kyla Levin, kateri.lalicker@tufts.edu")
+        .version("1.0")
+        .about("TMap Program")
+        .arg(Arg::new("config")
+            .short('c')
+            .long("config")
+            .value_name("FILE")
+            .takes_value(true))
+        .arg(Arg::new("INPUT")
+            .required(true)
+            .index(1))
+        .arg(Arg::new("v")
+            .short('v'))
+        .get_matches();
 
-        testFetaR.getattr("test_rwr")?.call0()?;
-        Ok(())
-    })
+    let mut file = File::create("output.txt").unwrap(); // Create output file with desired name
+    file.write_all(b"Hello, world!"); // Write results to output file
 
-    let mut file = File::create("output.txt")?; // Create output file with desired name
-    file.write_all(b"Hello, world!")?; // Write results to output file
+    // Python::with_gil(|py| {
+    //     let rarbanzo_r = PyModule::import(py, "Garbanzo").unwrap();
+    //     let rwr_r = PyModule::import(py, "RandomWalkWithRestart").unwrap();
 
-    // What format are the results of the algorithm be in???
-    // How to send that to output file?
+    //     // Testing purposes
+    //     let test_feta_r = PyModule::import(py, "test_feta").unwrap();
+    //     let x = test_feta_r.getattr("test_rwr").unwrap().call0().unwrap();
+    //     Ok(())
+    // });
 
-    Ok(())
+    Ok(());
 }
