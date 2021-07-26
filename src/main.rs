551de26f9bcb6ec14a44c3ae6a894dd3dd145af0
Kyla Levin <kyla.levin@gmail.com>
2021-07-26 10:06:11 -0400

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,141 @@
 mod utils;
-use std::fs::File;
-use std::io::prelude::*;
+// use std::fs::File;
 use pyo3::prelude::*; // Python importing
-use clap::Clap; // Command line handling
+use pyo3::ffi;
+use widestring::WideCString;
 use clap::{Arg, App};
 
-// extern crate clap;
-
-fn main() -> Result<T, E> {
-
+fn main() -> PyResult<()> {
+    
     /* Use a clap package to handle command line parsing and put into a struct */
-
     let matches = App::new("TMap Program")
-        .author("Kyla Levin, kateri.lalicker@tufts.edu")
-        .version("1.0")
-        .about("TMap Program")
-        .arg(Arg::new("config")
-            .short('c')
-            .long("config")
-            .value_name("FILE")
-            .takes_value(true))
-        .arg(Arg::new("INPUT")
-            .required(true)
-            .index(1))
-        .arg(Arg::new("v")
-            .short('v'))
-        .get_matches();
+    .author("Kyla Levin, kateri.lalicker@tufts.edu")
+    .version("1.0")
+    .about("TMap Program")
+    .arg(
+        Arg::new("disease")
+        .short('p')
+        .long("parkinsons")
+    )
+    .arg(
+        Arg::new("algorithm")
+        .required(true)
+        .takes_value(true)
+    )
+    .arg(
+        Arg::new("alpha")
+        .required(true)
+        .takes_value(true)
+    )
+    .arg(
+        Arg::new("k_value")
+        .required(true)
+        .takes_value(true)
+    )
+    .arg(
+        Arg::new("dataset")
+        .required(true)
+        .takes_value(true)
+    )
+    .get_matches();
 
-    let mut file = File::create("output.txt").unwrap(); // Create output file with desired name
-    file.write_all(b"Hello, world!"); // Write results to output file
+    let mut conda_command = "".to_owned();
+    // let mut tests = "".to_owned();
+    let mut imports = "".to_owned();
+    let mut config = "".to_owned();
+    let mut data_set = "".to_owned();
+    let mut model_set = "".to_owned();
+    let data = matches.value_of("dataset").unwrap();
+    let algo = matches.value_of("algorithm").unwrap();
+    let alpha = matches.value_of("alpha").unwrap();
+    let k_value = matches.value_of("k_value").unwrap();
 
-    // Python::with_gil(|py| {
-    //     let rarbanzo_r = PyModule::import(py, "Garbanzo").unwrap();
-    //     let rwr_r = PyModule::import(py, "RandomWalkWithRestart").unwrap();
+    let mut pd = false;
+    if matches.is_present("disease") {
+        // tests.push_str("print('Evaluating Parkinsons')\n");
+        pd = true;
+    }
+    // else {
+    //     tests.push_str("print('Evaluating Alzheimers')\n");
+    // }
 
-    //     // Testing purposes
-    //     let test_feta_r = PyModule::import(py, "test_feta").unwrap();
-    //     let x = test_feta_r.getattr("test_rwr").unwrap().call0().unwrap();
-    //     Ok(())
-    // });
+    // tests.push_str("print('Alpha value is ");
+    // tests.push_str(alpha);
+    // tests.push_str("')\n");
+    // tests.push_str("print('K value is ");
+    // tests.push_str(k_value);
+    // tests.push_str("')\n");
 
-    Ok(());
+    if data == "huri" {
+        imports.push_str("from t_map.garbanzo.huri import Huri\n");
+        // tests.push_str("print('Running huri dataset')\n");
+        if pd {
+            data_set.push_str("data = Huri('./data/parkinsons', with_hugo=True)\n");
+        }
+        else {
+            data_set.push_str("data = Huri('./data/alzheimers', with_hugo=True)\n");
+        }
+    }
+    else {
+        imports.push_str("print('Sorry, don't recognize that dataset.')\n");
+    }
+
+    imports.push_str(r#"
+from t_map.hummus.hummus import Hummus
+from t_map.hummus.hummus_score import HummusScore, ScoreTypes
+"#);
+
+    if algo == "rwr" {
+        imports.push_str("from t_map.feta.randomwalk import RandomWalkWithRestart, PreComputeRWR\n");
+        model_set.push_str("model = RandomWalkWithRestart(alpha=config['alpha'])\n");
+        // tests.push_str("print('Running RWR')\n");
+    }
+    else {
+        imports.push_str("print('Sorry, don't recognize that algorithm.')\n");
+    }
+
+    config.push_str("config = { 'alpha': ");
+    config.push_str(alpha);
+    config.push_str(", 'k': ");
+    config.push_str(k_value);
+    config.push_str(", }\n");
+
+    data_set.push_str(r#"scoring = HummusScore(score_type=ScoreTypes.TOP_K, k=config['k'])
+runner = Hummus(data, with_scoring=scoring)
+"#);
+    model_set.push_str(r#"genes = {data.get(i).name for i in range(len(data))}
+nodes = set(data.graph.nodes)
+print(len(genes.intersection(nodes)), len(genes))
+"#);
+
+    // conda_command.push_str(&tests);
+    conda_command.push_str(&imports);
+    conda_command.push_str(&config);
+    conda_command.push_str(&data_set);
+    conda_command.push_str(&model_set);
+
+    if let Some(PYTHONHOME) = std::env::var_os("CONDA_PREFIX") {
+        unsafe {
+            ffi::Py_SetPythonHome(
+                WideCString::from_str(PYTHONHOME.to_str().unwrap())
+                    .unwrap()
+                    .as_ptr() as *const i32,
+            );
+        }
+    }
+
+    pyo3::prepare_freethreaded_python();
+
+    // let gil = Python::acquire_gil();
+    // let py = gil.python();
+    Python::with_gil(|py|{
+        let result = py.run(&conda_command, None, None);
+
+    if let Err(ref err) = result {
+        println!("{:?}", err);
+    }
+
+    });
+    
+    Ok(())
 }
