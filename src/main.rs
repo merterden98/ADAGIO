02bad252a00ccaf0e923f4a9320e5800fe6ed833
Kyla Levin <kyla.levin@gmail.com>
2021-07-27 13:24:04 -0400

    Modified for Glider

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod utils;
-// use std::fs::File;
 use pyo3::prelude::*; // Python importing
 use pyo3::ffi;
 use widestring::WideCString;
@@ -23,6 +22,11 @@ fn main() -> PyResult<()> {
         .takes_value(true)
     )
     .arg(
+        Arg::new("withHugo")
+        .short('h')
+        .long("with_hugo")
+    )
+    .arg(
         Arg::new("alpha")
         .required(true)
         .takes_value(true)
@@ -39,80 +43,24 @@ fn main() -> PyResult<()> {
     )
     .get_matches();
 
-    let mut conda_command = "".to_owned();
-    // let mut tests = "".to_owned();
-    let mut imports = "".to_owned();
-    let mut config = "".to_owned();
-    let mut data_set = "".to_owned();
-    let mut model_set = "".to_owned();
-    let data = matches.value_of("dataset").unwrap();
-    let algo = matches.value_of("algorithm").unwrap();
-    let alpha = matches.value_of("alpha").unwrap();
-    let k_value = matches.value_of("k_value").unwrap();
+    let (imports_str, data_str) = set_data(
+        matches.value_of("dataset").unwrap(), 
+        matches.is_present("withHugo"), 
+        matches.is_present("disease")
+    );
 
-    let mut pd = false;
-    if matches.is_present("disease") {
-        // tests.push_str("print('Evaluating Parkinsons')\n");
-        pd = true;
-    }
-    // else {
-    //     tests.push_str("print('Evaluating Alzheimers')\n");
-    // }
+    let (imports_str, model_str) = set_algo(
+        matches.value_of("algorithm").unwrap(),
+        imports_str
+    );
 
-    // tests.push_str("print('Alpha value is ");
-    // tests.push_str(alpha);
-    // tests.push_str("')\n");
-    // tests.push_str("print('K value is ");
-    // tests.push_str(k_value);
-    // tests.push_str("')\n");
-
-    if data == "huri" {
-        imports.push_str("from t_map.garbanzo.huri import Huri\n");
-        // tests.push_str("print('Running huri dataset')\n");
-        if pd {
-            data_set.push_str("data = Huri('./data/parkinsons', with_hugo=True)\n");
-        }
-        else {
-            data_set.push_str("data = Huri('./data/alzheimers', with_hugo=True)\n");
-        }
-    }
-    else {
-        imports.push_str("print('Sorry, don't recognize that dataset.')\n");
-    }
-
-    imports.push_str(r#"
-from t_map.hummus.hummus import Hummus
-from t_map.hummus.hummus_score import HummusScore, ScoreTypes
-"#);
-
-    if algo == "rwr" {
-        imports.push_str("from t_map.feta.randomwalk import RandomWalkWithRestart, PreComputeRWR\n");
-        model_set.push_str("model = RandomWalkWithRestart(alpha=config['alpha'])\n");
-        // tests.push_str("print('Running RWR')\n");
-    }
-    else {
-        imports.push_str("print('Sorry, don't recognize that algorithm.')\n");
-    }
-
-    config.push_str("config = { 'alpha': ");
-    config.push_str(alpha);
-    config.push_str(", 'k': ");
-    config.push_str(k_value);
-    config.push_str(", }\n");
-
-    data_set.push_str(r#"scoring = HummusScore(score_type=ScoreTypes.TOP_K, k=config['k'])
-runner = Hummus(data, with_scoring=scoring)
-"#);
-    model_set.push_str(r#"genes = {data.get(i).name for i in range(len(data))}
-nodes = set(data.graph.nodes)
-print(len(genes.intersection(nodes)), len(genes))
-"#);
-
-    // conda_command.push_str(&tests);
-    conda_command.push_str(&imports);
-    conda_command.push_str(&config);
-    conda_command.push_str(&data_set);
-    conda_command.push_str(&model_set);
+    let conda_command = configure_and_compile(
+        matches.value_of("k_value").unwrap(),
+        matches.value_of("alpha").unwrap(),
+        imports_str,
+        data_str,
+        model_str
+    );
 
     if let Some(PYTHONHOME) = std::env::var_os("CONDA_PREFIX") {
         unsafe {
@@ -126,16 +74,107 @@ print(len(genes.intersection(nodes)), len(genes))
 
     pyo3::prepare_freethreaded_python();
 
-    // let gil = Python::acquire_gil();
-    // let py = gil.python();
     Python::with_gil(|py|{
         let result = py.run(&conda_command, None, None);
 
-    if let Err(ref err) = result {
-        println!("{:?}", err);
-    }
+        if let Err(ref err) = result {
+            println!("{:?}", err);
+        }
 
     });
     
     Ok(())
+}
+
+fn set_data(dataset: &str, hugo: bool, pd: bool) -> (String, String) {
+
+    let mut imports = "".to_owned();
+    let mut data = "".to_owned();
+
+    if dataset == "huri" {
+        imports.push_str("from t_map.garbanzo.huri import Huri\n");
+        if pd && hugo {
+            data.push_str("data = Huri('./data/parkinsons', with_hugo=True)\n");
+        }
+        else if pd {
+            data.push_str("data = Huri('./data/parkinsons', with_hugo=False)\n");
+        }
+        else if !pd && !hugo {
+            data.push_str("data = Huri('./data/alzheimers', with_hugo=False)\n");
+        }
+        else {
+            data.push_str("data = Huri('./data/alzheimers', with_hugo=True)\n");
+        }
+    }
+
+    else if dataset == "stringdb" {
+        imports.push_str("from t_map.garbanzo.stringdb import StringDB\n");
+        data.push_str("data = StringDB('./data/alzheimers')\n");
+    }
+
+    else {
+        imports.push_str("print('Sorry, don't recognize that dataset.')\n");
+    }
+
+    imports.push_str(r#"from t_map.hummus.hummus import Hummus
+from t_map.hummus.hummus_score import HummusScore, ScoreTypes
+"#);
+    data.push_str(r#"scoring = HummusScore(score_type=ScoreTypes.TOP_K, k=config['k'])
+runner = Hummus(data, with_scoring=scoring)
+"#);
+    
+    return (imports, data);
+}
+
+fn set_algo(algorithm: &str, mut imports: String) -> (String, String) {
+    
+    let mut model = "".to_owned();
+
+    if algorithm == "rwr" {
+        imports.push_str("from t_map.feta.randomwalk import RandomWalkWithRestart, PreComputeRWR\n");
+        model.push_str("model = RandomWalkWithRestart(alpha=config['alpha'])\n");
+    }
+    
+    else if algorithm == "glider" {
+        imports.push_str("from t_map.feta.glide import Glider\n");
+        model.push_str("model = Glider()\nmodel.setup(graph = data.graph)
+        ");
+    }
+    
+    else {
+        imports.push_str("print('Sorry, don't recognize that algorithm.')\n");
+    }
+
+    model.push_str(r#"genes = {data.get(i).name for i in range(len(data))}
+nodes = set(data.graph.nodes)
+
+for i, test_runner in enumerate(runner.with_cv("LOO")):
+	with test_runner as (disease_genes, graph, fn):
+		predictions = model(disease_genes, graph)
+		fn(predictions)
+	
+print(scoring.testing_summary())
+"#);
+
+    return (imports, model);
+}
+
+fn configure_and_compile(k_val: &str, alpha_val: &str, import: String, data: String, model: String) -> String {
+    
+    let mut config = "".to_owned();
+
+    config.push_str("config = { 'alpha': ");
+    config.push_str(alpha_val);
+    config.push_str(", 'k': ");
+    config.push_str(k_val);
+    config.push_str(", }\n");
+    
+    let mut command = "".to_owned();
+
+    command.push_str(&import);
+    command.push_str(&config);
+    command.push_str(&data);
+    command.push_str(&model);
+
+    return command;
 }
