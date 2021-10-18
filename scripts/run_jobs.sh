9ce14e0f4030d5aa3ac65449ed861c01d30313a6
Megan R. Gelement <mgelem01@login-prod-01.pax.tufts.edu>
2021-10-18 09:28:21 -0400

    updated runtime

--- scripts/run_jobs.sh
+++ scripts/run_jobs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J run_tissue_rwt
-#SBATCH --time=00-03:00:00
-#SBATCH --mem=60G
+#SBATCH --time=00-12:00:00
+#SBATCH --mem=128GB
 #SBATCH --output=MyJob.%j.%N.out  #saving standard output to file, %j=JOBID,%N=NodeName
 #SBATCH --error=MyJob.%j.%N.err   #saving standard error to file, %j=JOBID,%N=NodeName
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=megan.gelement@tufts.edu
 
 
-python tissue_reweight_by_algs_exp.py -t /cluster/tufts/cowenlab/mgelem01/E-MTAB-5214/tissue_files -g /cluster/tufts/cowenlab/mgelem01/tmap/data/alzheimers
+./tissue_reweight_by_algs_exp.py -t /cluster/tufts/cowenlab/mgelem01/E-MTAB-5214/tissue_files -g /cluster/tufts/cowenlab/mgelem01/tmap/data/alzheimers -a all
