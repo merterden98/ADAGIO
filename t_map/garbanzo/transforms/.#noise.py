c11085081e8efddddc1e6d76840f53a0261c5aee
Kapil Devkota <kdkapildevkota@gmail.com>
2022-04-12 13:41:19 -0400

    Added noise modifier

---
+++ t_map/garbanzo/transforms/.#noise.py
@@ -0,0 +1,1 @@
+kapil@kapil-ThinkPad-P52s.4335:1649782145
--- t_map/garbanzo/transforms/noise.py
+++ t_map/garbanzo/transforms/noise.py
@@ -1,7 +1,9 @@
 import networkx as nx
+from networkx.algorithms import tree
+import numpy as np
 
 
-def gen_noise(graph: nx.Graph, percent: float = 0.05) -> nx.Graph:
+def gen_noise(graph: nx.Graph, p: float = 0.05) -> nx.Graph:
     """
     Generate noise on the graph.
     :param graph:
@@ -14,4 +16,28 @@ def gen_noise(graph: nx.Graph, percent: float = 0.05) -> nx.Graph:
     # If p is greater than 0 (addition) -> randomly generate [(p, q, w)] 
     """
 
-    # TODO: Kapil
+    if p < 0:
+        """
+        Edge removal operation:
+        """
+        mst            = tree.maximum_spanning_edges(graph, algorithm="prim", data=False)
+        filtered_edges = graph.copy()
+        filtered_edges.remove_edges_from(list(mst)) 
+        for e in filtered_edges.edges():
+            pval = np.random.uniform()
+            if pval < np.abs(p): 
+                print("Remove")
+                graph.remove_edge(*e)
+    else:
+        edges_to_add  = int(p * graph.number_of_edges())
+        graph_nodes   = list(graph.nodes())
+        rand_tuples   = np.random.randint(len(graph_nodes), size = (2 * edges_to_add, 2))
+        count  = 0
+        for p_, q_ in rand_tuples:
+            p, q      = [graph_nodes[p_], graph_nodes[q_]]
+            if not graph.has_edge(p, q):
+                count += 1
+                graph.add_edge(p, q, weight = np.random.uniform())
+                if count >= edges_to_add:
+                    break
+    return graph
