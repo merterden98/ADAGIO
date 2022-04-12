1f799e388967113dffe83eecc8acba504e4b94a7
Mert Erden <merden01@eecs.tufts.edu>
2022-04-12 13:07:00 -0400

    feat(noise): generated scaffolding for creating noise

--- t_map/utils/interval_of_witness.py
+++ t_map/utils/interval_of_witness.py
@@ -1,5 +1,6 @@
 
 
+from collections import defaultdict
 from typing import Tuple, List
 from t_map.gene.gene import Gene
 
@@ -7,31 +8,32 @@ from t_map.gene.gene import Gene
 class Witness:
 
     def __init__(self, l_1: List[Tuple[Gene, float]],
-                 l_2: List[Tuple[Gene, float]]):
+                 *args: List[Tuple[Gene, float]]):
         self.l_1 = l_1
-        self.l_2 = l_2
+        self.l_2s = args
 
         self.l_1_map = dict()
-        self.l_2_map = dict()
+        self.l_2_map = defaultdict(list)
 
         for i, (g, _) in enumerate(l_1):
             self.l_1_map[g.name] = i
-        for i, (g, _) in enumerate(l_2):
-            self.l_2_map[g.name] = i
+        for l in self.l_2s:
+            for i, (g, _) in enumerate(l):
+                self.l_2_map[g.name].append(i)
 
     def find_range_of_witness(self, gene: Gene,
                               buffer=lambda x: x/2) -> Tuple[int, int]:
         start = list(filter(lambda x: x[1][0].name ==
                             gene.name, enumerate(self.l_1)))[0][0]
         end = start
-        while buffer(self.l_2_map[gene.name]) >= end:
+        while any(map(lambda x: buffer(x) >= end, self.l_2_map[gene.name])):
             end += 1
         return start, end
 
-    def find_all_witnesses(self) -> List[Tuple[Gene, int, int]]:
+    def find_all_witnesses(self, buffer=lambda x: x/2) -> List[Tuple[Gene, int, int]]:
         wittness_intervals = []
         for gene, _ in self.l_1:
-            start, end = self.find_range_of_witness(gene)
+            start, end = self.find_range_of_witness(gene, buffer)
             wittness_intervals.append((gene, start, end))
         return wittness_intervals
 
